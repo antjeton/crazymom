899c392065d4d5b06e08913613ca293dfc7cc6f4
antjeton <antjeton@yahoo.com>
2017-09-13 15:31:20 +0100

    js

---
+++ crazymom.js
@@ -0,0 +1,9 @@
+
+function calc(){
+var w =document.getElementbyId("width").value;
+var h = document.getElementbyId("height").value;
+
+var area =  w *h ;
+document.getElementbyId("answer").value=area;
+
+}
